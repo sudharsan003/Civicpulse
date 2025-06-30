76d1405f95d2767b1792252f6c8f3dc349698f09
sudharsan003 <sudhansudharsan003@gmail.com>
2025-07-01 03:32:19 +0530

    Fix: Remove recursive include from civicpulse/urls.py

--- civicpulse/urls.py
+++ civicpulse/urls.py
@@ -1,29 +1,15 @@
-"""
-URL configuration for civicpulse_project project.
+# civicpulse/urls.py
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path,include
-from django.conf import settings
-from django.conf.urls.static import static
-
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('civicpulse.urls')),
+    path('', views.home, name='home'),  # Replace `home` with your actual view name
+    path('report/', views.report_issue, name='report_issue'),
+    path('issues/', views.issue_list, name='issue_list'),
+    path('issue/<int:issue_id>/', views.issue_detail, name='issue_detail'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('register/', views.register_view, name='register'),
+    # add other views here
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
